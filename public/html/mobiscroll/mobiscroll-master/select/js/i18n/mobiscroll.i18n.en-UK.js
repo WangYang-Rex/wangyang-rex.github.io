635bfe8bface62b85ba105f71f2669f66db67cdf
wangyang <617418875@qq.com>
2020-01-10 09:15:43 +0800

    dev

--- public/html/mobiscroll/mobiscroll-master/select/js/i18n/mobiscroll.i18n.en-UK.js
+++
@@ -1,6 +0,0 @@
-(function ($) {
-    $.mobiscroll.i18n['en-GB'] = $.mobiscroll.i18n['en-UK'] = $.extend($.mobiscroll.i18n['en-UK'], {
-        dateFormat: 'dd/mm/yy',
-        dateOrder: 'ddmmy'
-    });
-})(jQuery);
